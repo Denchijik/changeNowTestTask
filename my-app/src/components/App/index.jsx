ad45600610933628e8e0bf5deed26955db0b5ea4
Denis Chizhik <denis.chizhik@actimind.com>
2020-08-30 20:09:43 +0300

    Init react app

---
+++ my-app/src/components/App/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+import ExchangeRate from '../ExchangeRate';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ExchangeRate />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/ExchangeRate/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css'
+
+const ExchangeRate= () => {
+    return (
+        <p>
+          Hi! 123
+        </p>
+    )
+}
+
+export default ExchangeRate;
